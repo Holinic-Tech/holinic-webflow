4961c804ac46975b7ab56f1c6283cb331f2d98a7
brainofcoco <brainofcoco2021@gmail.com>
2023-06-24 05:13:38 +0100

    testing js changes

--- hairqare/quiz-page/js/index.js
+++ hairqare/quiz-page/js/index.js
@@ -1,15 +1,14 @@
 // alert('Hello, Sam is testing js alert, test direct changes');
 // Get the checkbox element by ID
+// check box answer 1.
 const checkbox = document.getElementById('SplitEnds1');
 
 // Add a click event listener
 checkbox.addEventListener('click', function() {
   // Get the current value of the checkbox
-  const h = parseInt(checkbox.value);
-
+  console.log('clicked checkbox - value', checkbox.value);
   // Call the te(h) function with h - 1
-  te(2);
-  console.log('clicked checkbox - value', h);
+
 });
 
 // Function te(h)
