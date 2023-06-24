33ca7a2a2da69374cfad9998a0f660d28f6fd004
brainofcoco <brainofcoco2021@gmail.com>
2023-06-24 05:09:07 +0100

    pushing webflow test

--- hairqare/quiz-page/js/index.js
+++ hairqare/quiz-page/js/index.js
@@ -1,1 +1,15 @@
-alert('Hello, Sam is testing js alert, test direct changes');
+// alert('Hello, Sam is testing js alert, test direct changes');
+// Get the checkbox element by ID
+const checkbox = document.getElementById('SplitEnds1');
+
+// Add a click event listener
+checkbox.addEventListener('click', function() {
+  // Get the current value of the checkbox
+  const h = parseInt(checkbox.value);
+
+  // Call the te(h) function with h - 1
+  te(2);
+  console.log('clicked checkbox - value', h);
+});
+
+// Function te(h)
