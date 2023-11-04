799f992646f1205b61f4487bf4c642f15d80f1c9
TobyDie <53049174+TobyDie@users.noreply.github.com>
2023-11-04 14:59:18 +0800

    fixed redirect urls
    changed from WP to funnelish checkout

--- 4-the-quiz-haircare/js/main.js
+++ 4-the-quiz-haircare/js/main.js
@@ -371,7 +371,7 @@ function handleDataSubmission (email, firstName, lastName, answers) {
     // Prepare redirect URL
     var cvgUid = getCookieValue('__cvg_uid');
     // print(cvgUid, 'COOKIEEE TRACCKING'); - $39 checkout
-    var redirectUrl = 'https://checkout.hairqare.co/buy/hairqare-challenge-save-85/?__cvg_uid=' + cvgUid + '&billing_email=' + encodeURIComponent(email) + '&billing_first_name=' + encodeURIComponent(firstName) + '&billing_last_name=' + encodeURIComponent(lastName);
+    var redirectUrl = 'https://cart.hairqare.co/14-day-haircare-challenge?discount=save-85&__cvg_uid=' + cvgUid + '&billing_email=' + encodeURIComponent(email) + '&billing_first_name=' + encodeURIComponent(firstName) + '&billing_last_name=' + encodeURIComponent(lastName);
 
     // Post user's answers, name, and email to webhook - retry twicce
     $.ajax({
@@ -406,7 +406,7 @@ function handleDataSubmission (email, firstName, lastName, answers) {
 function handleSkipButton() {
     var cvgUid = getCookieValue('__cvg_uid');
     // print(cvgUid, 'COOKIEEE TRACCKING'); - $39 checkout
-    var redirectUrl = `https://checkout.hairqare.co/buy/hairqare-challenge-save-85/?__cvg_uid=${cvgUid}`
+    var redirectUrl = `https://cart.hairqare.co/14-day-haircare-challenge?discount=save-85&__cvg_uid=${cvgUid}`
     return window.top.location.href = redirectUrl;
 };
 
