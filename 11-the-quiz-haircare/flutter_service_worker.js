0e3d7e763dfb140548f68cd1e955dfb80f5e583e
Mohammed Mahfadi (Kali) <44398858+mohalmah@users.noreply.github.com>
2024-08-17 15:41:08 +0800

    new build with events fixed

--- 11-the-quiz-haircare/flutter_service_worker.js
+++ 11-the-quiz-haircare/flutter_service_worker.js
@@ -63,12 +63,12 @@ const RESOURCES = {"6480589508a7fb99895cef2e_BHC-Offerstack-flashy-leaves_LD.jpg
 "Email_profile_img.svg": "d1ee7fa14e2e1a0336047efacad9199d",
 "favicon.png": "2704101cb06ce66e2000356a312be25c",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "83c6306e670084d5984d7dd7d4addaeb",
+"flutter_bootstrap.js": "0c8d7e02dc8dc2e1cb378b7730bf62b8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "a266741b35a3a0832992813c81eb3fff",
 "/": "a266741b35a3a0832992813c81eb3fff",
-"main.dart.js": "3c4b012b828dde80f637be1dbabb6b01",
+"main.dart.js": "89a3c950e0445e0c874bb38b342773f9",
 "version.json": "81c18a1fc3bca3c183d40942ae8665fa"};
 // The application shell files that are downloaded before a service worker can
 // start.
